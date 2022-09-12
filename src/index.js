2d397f6c4d4487556d0d29788db9968f1f27c4ff
Trovarelli <jtneto1995@hotmail.com>
2022-09-12 17:20:45 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global-styles.css";
-import { Home } from "./template/Home";
+import { Home } from "./template/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
